d6e66f1a023366baba3a8491b296c8b000ccc6a5
grpawar1912 <110821470+grpawar1912@users.noreply.github.com>
2022-09-15 19:17:41 +0530

    Add files via upload

---
+++ program.py
@@ -0,0 +1,548 @@
+# from sys import ps1
+
+
+# print("hello world")
+# a=20
+# b=10.5
+# c=a+b
+# print(c)
+
+#x=input("enter value: ") #get value from user
+#y="hello "+ str(x)
+#print(y)
+
+# x="AWESOME"
+# y="i am "
+# def myfunc():
+#     x="i am "   
+#     print("python is  "+ x)
+
+# print(x)
+# print(y)
+# myfunc()
+
+# from curses import mouseinterval
+
+#program runs
+# x=" mine "
+# y="help me"
+
+# def myfunc():
+#     print("python is "+ x)
+# myfunc()
+# print(y)
+# print(x+y)
+# #task
+# n=0
+# while n<=3:
+#     print(n)
+#     n=n+1
+
+
+# Password is PythonIsHelpfull 
+
+# Take input password from adwale and check it he enters correct or incorrect and print as well
+
+
+
+
+
+
+
+
+2.# But he has another choice for password. If he enters the wrong password it will be his next password. 
+
+# Adwale is saying thanks to us 🙏
+#s=input("Enter password : ")
+# if s==PythonIs:
+#     print("correct")
+
+# else:
+#     print("incorrect")
+# p="gayu"
+# z=(input("enter your password " ))
+
+# while True:
+#     if z==p:
+#         print("login complete")
+#         break:
+    
+#     else:
+#         print("incorrect please enter correct password")
+        
+
+
+
+    
+   
+
+
+
+
+# Calculate Square of 15, 12.7, 24.01, 19 using if statement
+# [a,b,c,d]=[15,12.7,24.01,19]
+# if [a,b,c,d]==[15,12.7,24.01,19]:
+#     print(a*a,b*b,c*c,d*d)
+
+
+# num_list = []
+
+
+# while True:
+#     n = int(input('input a number: '))
+#     if n == 0:
+#         break
+#     num_list.append(n)
+#         sum_num = sum(num_list)
+#         ave_num = sum_num / len(num_list)
+#         print('The sum is: {}, and the average is: {}'.format(sum_num, ave_num))
+
+
+# avg = 0
+# sum = 0
+# count = 0
+# while True:
+#     num= int(input("enter number : "))
+#     if num!=0:
+#         sum=sum+num
+#         count=count+1
+#     else:
+#         print("Addition of numbers : ",sum)
+#         avg=sum/count
+#         print("Average of numbers : ",avg)
+#         break:
+
+
+# this program works
+# avg = 0
+# sum = 0
+# count = 0
+# while True:
+#     num=input("Enter number : ")
+#     if num==("finish","complete","done") or num=="4":
+#         break
+#     try:
+#         num=float(num)
+#         sum=sum+num
+#         count=count+1
+#         avg=sum/count
+#     except:
+#         print("invalid")
+    
+# print(sum,count,avg)
+
+# a=[{
+#     "name": "Gayatri",
+#     "program":"10",
+#     "onswitch": "20",
+#     "offswitch": "15"},
+#     {"name": "yogita",
+#     "program":"20",
+#     "onswitch": "30",
+#     "offswitch": "25"}]
+# for shift in a:
+#     if shift["program"]=="10":
+#         print(shift)
+# print("*****")
+# it= [13, 141, 112, 91, 174, 154]
+# wow=it[0]
+# for n in it:
+#     if n>=wow:
+#         wow=n
+#         print("iteration",n,wow)
+#     else:
+#         print("Iteration ",n,wow)
+        
+
+      
+# Output be like:
+
+# iteration 13 13 
+# iteration  141 141
+# iteration 112 141
+# iteration 91 141
+# iteration 174 174
+ # iteration 154 174
+# for templates
+# Themes wagan
+# themes forest
+# monster.com
+
+# input from user 6 times
+# arr=[]
+# s=1
+# for n in range(0,s):
+#     a=input("Enter your 6 numbers : ")
+#     if a=="finish":
+#         print("wrong number")
+#         break
+#     else :
+#         arr.append(a)
+# s=s+1
+# print(arr)
+# not run
+
+# if user enter a number example 5 fabio (ex5=5+4+3+2+1+0=15)
+#program runs
+# n=int(input("enter your number for factorial :"))
+# sum=0
+# for i in range(0,n+1):
+#     sum=sum+i
+#     print("how loop rotate",i)
+#     print("sum in loop",sum)
+# print("sum",sum)
+#without for loop  (works)
+# n=int(input("enter number"))
+# x=sum(range(0,n+1))
+# print(x)
+  
+#************************************************************************
+# x=int(input("enter your number"))
+# print(x.reverse())
+
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int)->List[int]:
+#         for i, value in enumerate(nums):
+#             remaining = target - nums[i]
+#             nums[i] = None
+#             if remaining in nums:
+#                 return [i, nums.index(remaining)]
+
+# name=['gaua','gau']
+# print(name)
+# z=[[23,2,3,4,22],["gau","gayu"]]
+# p=[9,90,7,44]
+# p.remove(90)
+# print(p)
+
+# tuple=(22,3,4,6)
+# tuple[2]
+# print(tuple[2])
+
+# n=int(input("Enter your number :"))
+# f=1
+# for i in range (0,n):
+#     f=n*f
+#     n=n-1
+# print(f)
+
+# g=int(input("enter your number to reverse:"))
+# rev=0
+# while g>0:
+#     rem=g%10
+#     rev=rev*10+rem  #alternate code rev=(rev*10)+i%10 i=i//10
+#     g=int(g/10)
+#print("reverse is: ",rev)
+
+
+# functions
+# m=max("learning python")
+
+# n=min("learning python")
+# print(m,n)
+# def my_function():
+#     print("hello gayatri")
+# my_function()
+
+# def add(x,y):
+#     return(x+y)
+# a=add(3,4)
+# print(a)
+
+# def takehelp(value):
+#     print(print("support "+ value))
+
+# takehelp("gayatri")
+# takehelp("gayu")
+# takehelp("3")
+
+# def year(f):
+#     for x in f:
+#         print(x)
+# jadu=("py","sy","ty")
+# jadu=(20,30,40)
+# year(jadu)
+   
+
+
+
+#************************************************
+# exercise
+
+# def func_tionout(a,b):
+#     subs=a-b
+#     adds=subs+24
+#     return subs,adds
+# print(func_tionout(20,10))
+    
+#number swap
+# a=int(input())
+# b=int(input())
+# a=a+b
+# b=a-b
+# a=a-b
+# print(a,b)
+
+#even od
+
+# num=int(input("enter your number : "))
+# if (num%2)==0:
+#     print("num is even")
+# else:
+#     print("num is odd")
+
+
+# Python program to check if the input number is odd or even.
+# A number is even if division by 2 gives a remainder of 0.
+# If the remainder is 1, it is an odd number.
+
+# num = int(input("Enter a number: "))
+# if (num % 2) == 0:
+#    print("num is Even")
+# else:
+#    print("num  is Odd")
+
+# Program to check if a number is prime or not
+
+# num =int(input("enter num "))
+
+# To take input from the user
+#num = int(input("Enter a number: "))
+
+# define a flag variable
+# flag = False
+
+# # prime numbers are greater than 1
+# if num > 1:
+#     # check for factors
+#     for i in range(2, num):
+#         if (num % i) == 0:
+#             # if factor is found, set flag to True
+#             flag = True
+#             # break out of loop
+#             break
+
+# # check if flag is True
+# if flag:
+#     print(num, "is not a prime number")
+# else:
+#     print(num, "is a prime number")
+
+
+# Python program to check if the number is an Armstrong number or not
+
+# # take input from the user
+# num = int(input("Enter a number: "))
+
+# # initialize sum
+# sum = 0
+
+# # find the sum of the cube of each digit
+# temp = num
+# while temp > 0:
+#    digit = temp % 10
+#    sum += digit ** 3
+#    temp //= 10
+
+# # display the result
+# if num == sum:
+#    print(num,"is an Armstrong number")
+# else:
+#    print(num,"is not an Armstrong number")
+
+
+# #functions
+# def helloworld():
+#     return "hello world"
+# print(helloworld()) 
+
+# def hello(sentence):
+#     return "hello"+" "+sentence
+# print(hello("gayatri"))
+# def addTwo(a, b):
+#     return a+b
+# print(addTwo(100,200))
+
+# #lists
+# first="a"
+# second="b"
+# third="c"
+# list_of_letters=["a","b","c","d","e"]
+# print(list_of_letters)
+# list_of_ints=[1,2,3,4,5,56]
+# list_of_floats=[1.0,1.99,2.0,3.0]
+# list_of_bools=[True,True,False]
+# print(list_of_ints ,list_of_floats, list_of_bools)
+# letters=["a","b","c","d","e"]
+# print(letters)
+# #counting starts from 0 in python
+# #["a","b","c","d","e"]
+# #[ 0,  1,   2,  3,  4]
+# #[-5  -4  -3  -2   -1]
+# print(letters[2])
+# print(letters[-1])
+
+
+# #slicingg lists
+# letters=["a","b","c","d","e"]
+# print(letters[1])
+# #we want to get "a" and "b"
+# print(letters[:2])
+# #we want to get "c"
+# print(letters[2:3])
+# #yourlist[start_index:end_index +1]
+
+# #we want to get "b" and everything else
+# print(letters[1:])
+
+# #List manipulation
+# letters=["a","b","c","d","e"]
+# #use len() to count items in list
+# print(len(letters))
+# #use  .append(newitem)
+# print(letters)
+# letters.append("f")
+# letters.append("g")
+# print(letters)
+# #use .pop() to remove items from back of list
+# letters.pop()
+# print(letters)
+
+# #you can store items popped out of list
+# poppeditem = letters.pop()
+# print(poppeditem)
+
+# letters.pop(0)#this pop "a" out of the list
+# print(letters)
+
+# #loop control
+# letters=["a","b","c","d","e"]
+# print(letters)
+# for letter in letters[1:4]:
+#     print(letter)
+# #range() gives you a range of numbers
+# numbers=range(8)#starts from 0 and ends eith 8-1
+# for number in numbers:
+#     print(number)
+# for number in range(9):# [0,1,2,.....,8]
+#     print("gayatri")
+
+
+
+# #loop control 2
+# numbers =range(9)
+# for number in numbers:
+#     if number == 5:
+# #        break #break the loop early
+# #        pass
+# #		continue #goes back to the top
+#     	print("the above let the code pass")
+#     else:
+        
+#         print(number)
+    
+# letters =["a","b","c","d","e"]
+
+# # while loop
+# test=0
+# while test<9:
+#     print("gayatri")
+#     test=1+test #adds 1 to test to make while definite loop
+
+
+# #dictionaries
+# #{key:value}
+# gayu={"name": "gayu",
+#       "age" : 18}
+# print(gayu)
+# gayu["country"]="India"
+# #we can retrive the value using the value
+# print(gayu["name"])
+# print(gayu.keys())
+# print(gayu.values())
+
+
+# #sets
+# example_set={"a","b","a"}
+# print(example_set)
+# example_list=[1,2,3,4,5,6]
+# print(set(example_list))
+# print(len(example_set))
+# example_set.add("f")
+# example_set.remove("a")
+
+# print(example_set)
+# example_set_2={"c","d","a"}
+# print(example_set_2)
+# print(example_set - example_set_2)
+
+
+
+
+#  Rewrite your pay computation with time-and-a-half for overtime 
+# and create a function called computepay which takes two parameters
+#  (hours and rate).
+
+# hours = int(input('Enter hours:'))#for overtime 
+# rate =  int(input('Enter rate:')) #two parameters hours and rate
+# pay =('Your pay this month ' + str((hours + hours/2) * rate))
+# def computepay(hours,rate):
+#    pay = ('Your pay this month :  ' + str((hours + hours/2) * rate))
+#    return pay 
+
+# print(pay)
+
+
+
+#file handling
+#handle=open(filename,mode ('r','w'))
+# handle=open('mypylearning.txt')
+# print(handle)
+# a=0
+# count=0
+# handle=open('mypylearning.txt')
+# for data in handle :
+#    # print(len(data))
+#    count=len(data)+count
+#    print(count)
+# def readfun():
+# handle=open('mypylearning.txt')
+# for data in handle:
+#    if data.startswith("From"):
+#       print(data.rstrip())#for strip(space) the space between lines
+
+# def readfun(fname,start,key):
+#    handle=open(fname)
+#    for data in handle:
+#       if (data.startswith(start) and not key in data):
+#          print(data)
+
+
+# readfun('mypylearning.txt',"From","Jan")
+
+# handle=open('mypylearning.txt')
+# data=handle.read()
+# print(data)
+
+   # a+=1
+   # print(a)
+   # print(data)
+# a='x\ny'  #lenght is 3 becoz \n consider 1 length value
+# print(len(a))
+
+# files  
+# 1.open a file
+# 2.handle a file
+# 3.new line 
+# 4.reading file using for loop and read method 
+# 5.counting(characters,line)
+# 6.length using len()function
+# 7.filename.startswith()
+# 8.if loop ,continue ,break,function
+# 9.input files
+
+
+
+
+
+
